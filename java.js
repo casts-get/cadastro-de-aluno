227b6d9e07eabecf9b3687791fae426a3aa3a870
carine_dos-santos@estudante.sesisenai.org.br <carine_dos-santos@estudante.sesisenai.i>
2026-05-24 23:26:32 -0300

    add- fiz a parte de validação do codigo

--- java.js
+++ java.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.needs-validation')
 
+form.addEventListener('submit', function(event){
+
+
+    const senha = document.getElementById('senha').value
+    const confirmarSenha = document.getElementById('confirmarSenha').value
+
+
+    if(!form.checkValidity()){
+        event.preventDefault()
+        event.stopPropagation()
+    }
+
+
+    if(senha !== confirmarSenha){
+        alert("As senhas não coincidem!")
+
+        event.preventDefault()
+        event.stopPropagation()
+    }
+
+
+    form.classList.add('was-validated')
+
+})
